3697c1e667392c971da26b08fa080b8899aa8f04
zainab.alkaleefa <zainab.alkaleefa@gmail.com>
2020-08-07 00:13:57 +0100

    add codewars api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{ useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [data, setdata] = useState([]);
+   ////////////////////// Get tasks  //////////////////////
+   useEffect(() => {
+    axios
+      .get('https://www.codewars.com/api/v1/users/zainabalkaleefa')
+      .then(//request is successful
+        response => {
+          console.log(response.honor);
+          const honorNumber = response.data.honor;
+          setdata(honorNumber);
+        })
+      .catch(// an error
+        (error) => {
+          console.log('Error getting data', error)
+        }
+      )
+      .finally(() => console.log("I am done"))
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="background">
+     {data}
     </div>
   );
 }
