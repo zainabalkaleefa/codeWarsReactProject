966e12e15371b1f40631744b21e9cdd5a7b138d9
zainab.alkaleefa <zainab.alkaleefa@gmail.com>
2020-08-18 02:29:43 +0100

    fetch all the required data from codewars api

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import './App.css';
 function App() {
   const [data, setdata] = useState([]);
   const[name, setName]=useState([]);
+  const[userName, setuserName]=useState([]);
   const[rank, setRank]=useState([]);
   const[skill, setSkill]=useState([]);
+  const[challenge, setChallenge]=useState([]);
    ////////////////////// Get tasks  //////////////////////
    useEffect(() => {
     axios
@@ -17,13 +19,17 @@ function App() {
         response => {
           const honorNumber = response.data.honor;
           const Name = response.data.name;
+          const UserName = response.data.username;
           const Rank = response.data.ranks.overall.name;
           const Skill = response.data.skills;
+          const Challenge = response.data.codeChallenges.totalCompleted;
           console.log(honorNumber);
           setdata(honorNumber);
           setName(Name);
           setRank(Rank);
           setSkill(Skill);
+          setChallenge(Challenge);
+          setuserName(UserName);
 
         })
       .catch(// an error
@@ -39,11 +45,12 @@ function App() {
 <div class="card bg-dark text-white">
   <img src="https://pathrise-website-guide-wp.s3.us-west-1.amazonaws.com/guides/wp-content/uploads/2019/06/10173017/codewars-logo-1.png" className="card-img img" alt="..." style={{height: '100vh'}}/>
   <div class="card-img-overlay cardPosition">
-    <h2 class="card-title" align ="center">zainabalkaleefa</h2>
-    <h3 class="card-text" align="center">Honor:{data}</h3>
+    <h1 class="card-title" align ="center">{userName}</h1> <br></br><br></br>
     <h3 class="card-text" align="center">Name:{name}</h3>
+    <h3 class="card-text" align="center">Honor:{data}</h3>
     <h3 class="card-text" align="center">Rank:{rank}</h3>
     <h3 class="card-text" align="center">Laguages:{skill}</h3>
+    <h3 class="card-text" align="center">Completed Challenges:{challenge}</h3>
    
    
   </div>
