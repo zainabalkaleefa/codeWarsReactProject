42841bcd52002cf77904f0326d24de784fb548c1
zainab.alkaleefa <zainab.alkaleefa@gmail.com>
2020-08-20 00:27:42 +0100

    add input field and submit button

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ function App() {
    useEffect(() => {
     axios
       // .get('https://api.github.com/users/zainabalkaleefa') // github api
-      .get("/api/v1/users/zainabalkaleefa/?access_key=i8J6XHhoBAdVH44bJ3-p")
+      // .get("/api/v1/users/zainabalkaleefa/?access_key=i8J6XHhoBAdVH44bJ3-p")
+      .get("/api/v1/users/zainabalkaleefa")
       .then(//request is successful
         response => {
           const honorNumber = response.data.honor;
@@ -41,11 +42,32 @@ function App() {
   }, [setdata]);
 
   return (
-
+<div>
+         
 <div class="card bg-dark text-white">
   <img src="https://pathrise-website-guide-wp.s3.us-west-1.amazonaws.com/guides/wp-content/uploads/2019/06/10173017/codewars-logo-1.png" className="card-img img" alt="..." style={{height: '100vh'}}/>
   <div class="card-img-overlay cardPosition">
-    <h1 class="card-title" align ="center">{userName}</h1> <br></br><br></br>
+          <input
+            type="text"
+            className="form-control  inputField"
+            name="text_name" placeholder=" Add a new Task"
+            style={{ width: "30%" }}
+            // aria-describedby="inputGroup-sizing-Default"
+            // onChange={textChange}
+            // value={text} 
+            />
+        
+          <button 
+            type="button"
+            className="btn btn-danger addField"
+            align="center"
+            // onClick={addTaskButton}
+            style={{ width: "10%" }}
+          >  Submit  </button>
+     
+     
+    {/* <h1 class="card-title" align ="center">{userName}</h1>  */}
+    <br></br><br></br>
     <h3 class="card-text" align="center">Name:{name}</h3>
     <h3 class="card-text" align="center">Honor:{data}</h3>
     <h3 class="card-text" align="center">Rank:{rank}</h3>
@@ -54,6 +76,7 @@ function App() {
    
    
   </div>
+</div>
 </div>
   );
 }
