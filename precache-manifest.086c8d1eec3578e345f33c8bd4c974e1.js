d8dd7f2be1ab308129f143a5284a1a324e3011d8
zainab.alkaleefa <zainab.alkaleefa@gmail.com>
2020-10-15 02:10:39 +0100

    Updates

---
+++ precache-manifest.086c8d1eec3578e345f33c8bd4c974e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13191b6f7f3669487b0c3d55f302e92c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37cdd74bbe8f6f62f102",
+    "url": "/static/css/2.a09d231f.chunk.css"
+  },
+  {
+    "revision": "157ada682c583389979e",
+    "url": "/static/css/main.ea3cf2a8.chunk.css"
+  },
+  {
+    "revision": "37cdd74bbe8f6f62f102",
+    "url": "/static/js/2.5cc3927a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5cc3927a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "157ada682c583389979e",
+    "url": "/static/js/main.04726288.chunk.js"
+  },
+  {
+    "revision": "5c0c593ab07f2602547a",
+    "url": "/static/js/runtime-main.e9e3dd4f.js"
+  }
+]);
